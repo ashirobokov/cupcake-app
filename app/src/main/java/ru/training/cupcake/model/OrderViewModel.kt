da39a92d260ee908b4a5ee375541b9b44193dbe0
Andrey Shirobokov <loadtest@mail.ru>
2023-02-28 11:55:58 +0300

    added OrderViewModel unit testing.

--- app/src/main/java/ru/training/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/ru/training/cupcake/model/OrderViewModel.kt
@@ -7,20 +7,19 @@ import androidx.lifecycle.ViewModel
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.logging.SimpleFormatter
 
 private const val PRICE_PER_CUPCAKE = 2.00
 private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
-class OrderViewModel: ViewModel() {
+class OrderViewModel : ViewModel() {
     val dateOptions = getPickUpOptions()
     private val _quantity = MutableLiveData<Int>(0)
     val quantity: LiveData<Int>
-            get() = _quantity
+        get() = _quantity
 
     private val _flavor = MutableLiveData<String>("")
     val flavor: LiveData<String>
-            get() = _flavor
+        get() = _flavor
 
     private val _date = MutableLiveData<String>("")
     val date: LiveData<String>
@@ -36,7 +35,7 @@ class OrderViewModel: ViewModel() {
     }
 
     private fun getPickUpOptions(): List<String> {
-        val options = mutableListOf <String>()
+        val options = mutableListOf<String>()
         val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
         val calendar = Calendar.getInstance()
 
@@ -49,7 +48,7 @@ class OrderViewModel: ViewModel() {
     }
 
     private fun updatePrice() {
-        var calculatedPrice = (quantity.value?: 0) * (PRICE_PER_CUPCAKE)
+        var calculatedPrice = (quantity.value ?: 0) * (PRICE_PER_CUPCAKE)
         if (date.value == dateOptions[0]) {
             calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
         }
